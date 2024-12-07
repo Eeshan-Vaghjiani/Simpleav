80ab3528a888d69875b3b88bf8b00d8972028d17
Eeshan-vaghjiani <evaghjiani@gmail.com>
2024-12-07 12:15:18 +0300

    index

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 define('CONSUMER_KEY', 'your_consumer_key');
 define('CONSUMER_SECRET', 'your_consumer_secret');
 define('LIPA_NA_MPESA_URL', 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1'); // Use production URL for live
-define('SHORTCODE', 'your_shortcode'); // Your shortcode
+define('SHORTCODE', 'your_shortcode');
 define('LIPA_NA_MPESA_PASSKEY', 'your_passkey');
 define('LIPA_NA_MPESA_SHORTCODE', 'your_shortcode');
-define('CALLBACK_URL', 'https://your-ngrok-url/callback.php'); // Set your ngrok URL here
+define('CALLBACK_URL', 'https://your-ngrok-url/callback.php');
